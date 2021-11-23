41671be4ba4695486f22aa6b3f6a489121aa3396
wildcrow-sky <wildcrow@list.ru>
2021-11-23 16:07:36 +0300

    Lesson3 - 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,26 @@
+"use strict";
+
+// Проверить 2 массива - длина разная и не равны нулю
+
 function compareArrays(arr1, arr2) {
-  let result;
+  let result; // Определяем переменную результата
+  let arr = []; // Определяем пустой массив для обработки 2 входящих массивов
 
-  // Ваш код
+  if (arr1.length === 0 && arr2.length === 0 || arr1.length != arr2.length) return false; // Проверяем массивы на длину и наличие данных
 
+  for (let i = 0; i < arr1.length; i ++) {
+    arr.push(arr1[i] - arr2[i]);
+  }
+  
+  result = arr.every(elem => elem === 0);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  
+  if (arr.length === 0) return `Массив arr - пустой`;
 
-  // Ваш код
-
+  let resultArr = arr.filter(elem => elem > 0 && elem % 3 === 0).map(elem => elem * 10);
   return resultArr; // array
+  
 }
