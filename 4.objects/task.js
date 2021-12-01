2b80146f90d5d34f74a20077f3f28251932ed8ac
wildcrow-sky <wildcrow@list.ru>
2021-12-01 20:05:00 +0300

    Lesson4 - 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
-function Student(name, gender, age) {
-    // Ваш код
+"use strict";
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+  console.log(this.marks.length); // Удалить
+
+}
+
+Student.prototype.addMarks = function (mark1, ...mark) {
+
+  if(this.marks === undefined) { 
+    this.marks = [mark1];
+  } else {
+    this.marks.push(mark1);
+  }
+  this.marks = this.marks.concat(mark); // такое решение добавлено на тот случай, если на вход подается метод addMark и addMarks
+  
+}
+
+Student.prototype.getAverage = function() {
+
+  return this.marks.reduce((previousValue, currentValue) => previousValue + currentValue) / this.marks.length;
+
+}
+
+Student.prototype.exclude = function(reason) {
+
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+  
+}
