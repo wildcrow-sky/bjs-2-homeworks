4384ef374e678e6079a83c08803b78d07d952cdd
wildcrow-sky <wildcrow@list.ru>
2021-12-09 13:31:27 +0300

    Lesson6 - 6.exception-closure

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,49 @@
+"use strict"
+
+// Задача №1. Форматтер чисел
+
+function parseCount(count) {
+  let digitCount = Number.parseInt(count);
+  if (isNaN(digitCount)) throw new Error("Невалидное значение");
+  return digitCount;
+}
+
+function validateCount(count) {
+  try {
+    return parseCount(count);
+  } catch (error) {
+    return error;
+  }
+}
+
+// Задача №1. Треугольник
+
+class Triangle {
+  constructor(a, b, c) {
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    } else throw new Error("Треугольник с такими сторонами не существует");
+  }
+
+  getPerimeter() {
+    return this.perimetr = this.a + this.b + this.c;
+  }
+
+  getArea() {
+    let p = this.perimetr / 2;
+    return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3))
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch(error) {
+    return {
+      getPerimeter() {return "Ошибка! Треугольник не существует";},
+      getArea() {return "Ошибка! Треугольник не существует";}
+    }
+  }
+}
